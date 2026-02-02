898d53601531d40cab58c24e014c1559d6270c43
Tayl0rH <taylorher0601@gmail.com>
2026-02-02 18:18:50 +0100

    Proyecteando

---
+++ angular-event3/src/app/event-service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { IEvent } from './interfaces/i-event';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EventService {
+
+  events: IEvent[] = [
+      {
+        title: 'Discoteca spook',
+        image: '/images/spook.jpg',
+        date: '2026-04-23',
+        description:
+          'Spook es un icono de la noche electrónica, donde música, luces y energía se fusionan en una experiencia única que marca generaciones y convierte cada noche en algo inolvidable.',
+        price: 23,
+      },
+      {
+        title: 'Boda mujer contra mujer',
+        image: '/images/boda.jpg',
+        date: '2026-02-10',
+        description:
+          'Una boda es la celebración del amor, un día especial donde se unen emociones, familia y amigos para compartir momentos únicos que quedarán grabados para siempre.',
+        price: 59.99,
+      },
+      {
+        title: 'Festival de música urbana',
+        image: '/images/urbana.jpg',
+        date: '2026-06-15',
+        description:
+          'Un festival lleno de ritmo y energía donde los mejores artistas urbanos se reúnen para ofrecer una experiencia musical intensa y vibrante al aire libre.',
+        price: 35,
+      },
+      {
+        title: 'Concierto acústico íntimo',
+        image: '/images/intimo.jpg',
+        date: '2026-03-05',
+        description:
+          'Un concierto cercano y especial en un ambiente reducido, donde la música en directo y la conexión con el artista crean una experiencia única.',
+        price: 18,
+      },
+      {
+        title: 'Evento corporativo networking',
+        image: '/images/networking.jpg',
+        date: '2026-05-20',
+        description:
+          'Un encuentro profesional pensado para conectar ideas, personas y oportunidades en un entorno dinámico, moderno y cuidadosamente organizado.',
+        price: 0,
+      },
+    ];
+
+  getEvents(): IEvent[]{
+    return [...this.events];
+  }
+}
---
+++ angular-event3/src/app/pipes/title-filter-pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { IEvent } from '../interfaces/i-event';
+
+@Pipe({
+  name: 'titleFilter',
+})
+export class TitleFilterPipe implements PipeTransform {
+
+  transform(events: IEvent[], filterBy: string): IEvent[] {
+
+    const filter = filterBy ?  filterBy.toLocaleLowerCase() : null;
+
+    return  filter
+            ? events.filter(
+                (event) => event.title.toLocaleLowerCase().includes(filter)
+              )
+            : events;
+
+  }
+
+}
